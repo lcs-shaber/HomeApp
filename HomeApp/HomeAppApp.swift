e999662ebefe9771718219d389ddc71edc4e7c31
Sophie Haber <SHaber@lcs.on.ca>
2024-01-10 11:43:52 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Sophie Haber on 10.01.24.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
